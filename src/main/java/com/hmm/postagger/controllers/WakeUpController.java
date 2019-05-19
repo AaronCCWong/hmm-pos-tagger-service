35fefb145fad0502d70058a95676ba35777c38ef
AaronCCWong <aarn.wong@gmail.com>
2019-05-18 23:09:31 -0400

    Allow request from any origin to wakeUp

--- src/main/java/com/hmm/postagger/controllers/WakeUpController.java
+++ src/main/java/com/hmm/postagger/controllers/WakeUpController.java
@@ -1,11 +1,13 @@
 package com.hmm.postagger.controllers;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WakeUpController {
 
+    @CrossOrigin
     @GetMapping("/wakeUp")
     public void wakeUp() {}
 
