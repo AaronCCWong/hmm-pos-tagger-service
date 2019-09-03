1aa886acc20f80bf1fb0299f48a498b63b1a8af9
AaronCCWong <aarn.wong@gmail.com>
2019-09-03 19:55:34 -0400

    Return 204 when wakeUp is called

--- src/main/java/com/hmm/postagger/controllers/WakeUpController.java
+++ src/main/java/com/hmm/postagger/controllers/WakeUpController.java
@@ -1,7 +1,9 @@
 package com.hmm.postagger.controllers;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,6 +11,7 @@ public class WakeUpController {
 
     @CrossOrigin
     @GetMapping("/wakeUp")
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
     public void wakeUp() {}
 
 }
